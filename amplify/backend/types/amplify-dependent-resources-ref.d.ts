32b0bb698e9137ad99db49c439e7977bb2be0764
damianmiskow <111329721+damianmiskow@users.noreply.github.com>
2024-01-26 12:25:12 -0500

    LoginPage Working

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "synchronyinterviews1bed362c": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
